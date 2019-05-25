27dbddda466163d7aaab077239a7fba05824e77b
Shah <uid38717@contiwan.com>
2019-05-25 16:54:15 +0530

    upate on data analysis script

--- analyze_mura_dataset.py
+++ analyze_mura_dataset.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # MURA Dataset - Exploratory Data Analysis
+
+# ### Import the required libraries
+
+# In[1]:
+
+
 import os
 
 import numpy as np
@@ -6,6 +16,12 @@ import pandas as pd
 import random
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+
+# ### Load the data files
+
+# In[2]:
+
 
 base_dir = 'MURA-v1.1'
 train_file = 'train_data_files.csv'
@@ -17,23 +33,51 @@ xrays_cat = os.listdir(os.path.join(base_dir,'train'))
 train_df = pd.read_csv(os.path.join(base_dir,train_file),usecols=['image_path','class_name','class_label','body_part','study_type'])
 val_df = pd.read_csv(os.path.join(base_dir,val_file),usecols=['image_path','class_name','class_label','body_part','study_type'])
 
-#%%
-# EDA of train data
+
+# ### See the first few samples of train and val dataframe
+
+# In[6]:
+
+
+# thsi will print the first 3 samples
+print ('Training dataframe')
+print (train_df.head(3))
+print ('-------------------------------------------------------------------------------')
+print ('validation dataframe')
+print (val_df.head(3))
+
+
+# ## EDA of Training Data
+
+# ### Plot to see the count of positive and negative samples in the dataset
+
+# In[12]:
+
+
+# Filter out the poistive and negative samples from the dataframe
 positive_sample_tr = train_df[train_df['class_label']==1]
 negative_sample_tr = train_df[train_df['class_label']==0]
 
 num_pos_samples_tr = len(positive_sample_tr)
 num_neg_samples_tr = len(negative_sample_tr)
+print ('num of postive samples in training data: ', num_pos_samples_tr)
+print ('num of negative samples in training data: ', num_neg_samples_tr)
 
 #dist = train_df['labels'].value_counts
 ax=sns.countplot(train_df['class_name'])
 total = len(train_df)
 for p in ax.patches:
     height = p.get_height()
-    ax.text(p.get_x()+p.get_width()/2.,height+3.5 ,
-            '{},({:1.2f})'.format(height,(height/total)),
+    ax.text(p.get_x()+p.get_width()/2.,height+10 ,
+            '{}, ({:1.2f})'.format(height,(height/total)),
             ha="center")
-#%%
+
+
+# ### Visualize some positive samples from training data
+
+# In[13]:
+
+
 # read some of positive samples
 fig = plt.figure(figsize=(12,12))
 for i in range(9):
@@ -50,7 +94,13 @@ for i in range(9):
   plt.yticks([])
 fig
 
-# read some of positive samples
+
+# ### Visualize some negative samples from training data
+
+# In[14]:
+
+
+# read some of negative samples
 fig = plt.figure(figsize=(12,12))
 for i in range(9):
   ind =  random.randint(0,len(negative_sample_tr))
@@ -65,7 +115,13 @@ for i in range(9):
   plt.xticks([])
   plt.yticks([])
 fig
-#%%
+
+
+# ### Plot to visualize the distribution of positive and negative samples over different body parts
+
+# In[19]:
+
+
 plt.figure(figsize=(12,7))
 ax=sns.countplot(data=train_df,x='body_part',hue='class_name')
 #total=len(plots[i-1])
@@ -75,17 +131,11 @@ for p in ax.patches:
             height+3.5 ,
             '{}'.format(height),
             ha="center")
-#%%
-plt.figure(figsize=(12,7))
-ax=sns.countplot(data=train_df,x='study_type',hue='class_name')
-#total=len(plots[i-1])
-for p in ax.patches:
-    height = p.get_height()
-    ax.text(p.get_x()+p.get_width()/2.,
-            height+3.5 ,
-            '{}'.format(height),
-            ha="center")
-#%%
+
+
+# In[18]:
+
+
 elbow_df_tr = train_df[train_df['body_part']=='ELBOW']
 finger_df_tr = train_df[train_df['body_part']=='FINGER']
 forearm_df_tr = train_df[train_df['body_part']=='FOREARM']
@@ -116,24 +166,52 @@ for i in range(1,8):
 plt.show()
 
 
-#%%  
-# EDA of validation data
+# ### Plot to visualize the distribution of positive and negative samples over different study types
+
+# In[17]:
+
+
+plt.figure(figsize=(12,7))
+ax=sns.countplot(data=train_df,x='study_type',hue='class_name')
+#total=len(plots[i-1])
+for p in ax.patches:
+    height = p.get_height()
+    ax.text(p.get_x()+p.get_width()/2.,
+            height+3.5 ,
+            '{}'.format(height),
+            ha="center")
+
+
+# ## EDA of Validation Data
+
+# ### Plot to see the count of positive and negative samples in the dataset
+
+# In[22]:
+
+
 positive_sample_val = val_df[val_df['class_label']==1]
 negative_sample_val = val_df[val_df['class_label']==0]
 
 num_pos_samples_val = len(positive_sample_val)
 num_neg_samples_val = len(negative_sample_val)
+print ('number of positive samples in validation data: ',num_pos_samples_val)
+print ('number of negative samples in validation data: ',num_neg_samples_val)
 
 #dist = train_df['labels'].value_counts
 ax = sns.countplot(val_df['class_name'])
 total = len(val_df)
 for p in ax.patches:
     height = p.get_height()
-    ax.text(p.get_x()+p.get_width()/2.,height+3.5 ,
-            '{},({:1.2f})'.format(height,(height/total)),
+    ax.text(p.get_x()+p.get_width()/2.,height+10 ,
+            '{}, ({:1.2f})'.format(height,(height/total)),
             ha="center")
-    
-#%%
+
+
+# ### Read some positive samples from the validation data
+
+# In[23]:
+
+
 # read some of positive samples
 fig = plt.figure(figsize=(12,12))
 for i in range(9):
@@ -149,6 +227,12 @@ for i in range(9):
   plt.xticks([])
   plt.yticks([])
 fig
+
+
+# ### Read some negative samples from the validation data
+
+# In[24]:
+
 
 # read some of positive samples
 fig = plt.figure(figsize=(12,12))
@@ -166,7 +250,25 @@ for i in range(9):
   plt.yticks([])
 fig
 
-#%%
+
+# ### Plot to visualize the distribution of positive and negative samples over different body parts
+
+# In[25]:
+
+
+plt.figure(figsize=(12,7))
+ax=sns.countplot(data=train_df,x='body_part',hue='class_name')
+#total=len(plots[i-1])
+for p in ax.patches:
+    height = p.get_height()
+    ax.text(p.get_x()+p.get_width()/2.,
+            height+3.5 ,
+            '{}'.format(height),
+            ha="center")
+
+
+# In[26]:
+
 
 elbow_df_val = val_df[val_df['body_part']=='ELBOW']
 finger_df_val = val_df[val_df['body_part']=='FINGER']
@@ -176,20 +278,19 @@ humerus_df_val = val_df[val_df['body_part']=='HUMERUS']
 shoulder_df_val = val_df[val_df['body_part']=='SHOULDER']
 wrist_df_val = val_df[val_df['body_part']=='WRIST']
 
-plots_val = [elbow_df_val,finger_df_val,forearm_df_val,\
-         hand_df_val,humerus_df_val,shoulder_df_val,wrist_df_val]
+plots = [elbow_df_val,finger_df_val,forearm_df_val,hand_df_val,humerus_df_val,shoulder_df_val,wrist_df_val]
 
 # plot with various axes scales
 fig = plt.figure(figsize=(14,14))
 fig.subplots_adjust(hspace=0.4, wspace=0.4)
 for i in range(1,8):
     plt.subplot(3,3,i)
-    ax = sns.countplot(x='class_name',data=plots_val[i-1])
-    total=len(plots_val[i-1])
+    ax = sns.countplot(x='class_name',data=plots[i-1])
+    total=len(plots[i-1])
     for p in ax.patches:
         height = p.get_height()
         ax.text(p.get_x()+p.get_width()/2.,
-            height+3 ,
+            height+3.5 ,
             '{},({:1.2f})'.format(height,(height/total)),
             ha="center")
 #    plt.legend('neg','pos')
@@ -197,6 +298,23 @@ for i in range(1,8):
 #    plt.grid(True)
 #plt.legend('neg','pos')
 plt.show()
+
+
+# ### Plot to visualize the distribution of positive and negative samples over different study types
+
+# In[27]:
+
+
+plt.figure(figsize=(12,7))
+ax=sns.countplot(data=train_df,x='study_type',hue='class_name')
+#total=len(plots[i-1])
+for p in ax.patches:
+    height = p.get_height()
+    ax.text(p.get_x()+p.get_width()/2.,
+            height+3.5 ,
+            '{}'.format(height),
+            ha="center")
+
 
 #%%
 #['XR_ELBOW', 'XR_FINGER', 'XR_FOREARM', 'XR_HAND', 'XR_HUMERUS', 'XR_SHOULDER', 'XR_WRIST']
@@ -236,6 +354,8 @@ humerus_df_tr,shoulder_df_tr,wrist_df_tr        =   get_individual_parts(train_d
 
 elbow_df_val,finger_df_val,forearm_df_val,hand_df_val,\
 humerus_df_val,shoulder_df_val,wrist_df_val        =   get_individual_parts(val_df)
+
+
 
 
 
